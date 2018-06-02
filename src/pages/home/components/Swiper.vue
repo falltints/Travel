3d99b8fe3ae165638f6b6e682d6d0645eccc49b8
falltints <2832731037@qq.com>
2018-06-02 20:05:38 +0800

    modify

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -16,7 +16,9 @@ export default {
     return {
       swiperOption: {
         pagination: '.swiper-pagination',
-        loop: true
+        loop: true,
+        observer: true,
+        observeParents: true
       },
       swiperList: [{
         id: '001',
