3381bed3880e5526472d4461f0031870fcf2212d
falltints <2832731037@qq.com>
2018-06-02 19:18:10 +0800

    change

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,27 +1,17 @@
 <template>
 <div>
   <home-header></home-header>
-<<<<<<< HEAD
-=======
   <home-swiper></home-swiper>
->>>>>>> origin/index-swiper
 </div>
 </template>
 <script>
 import HomeHeader from './components/Header'
-<<<<<<< HEAD
-export default {
-  name: 'Home',
-  components: {
-    HomeHeader
-=======
 import HomeSwiper from './components/Swiper'
 export default {
   name: 'Home',
   components: {
     HomeHeader,
     HomeSwiper
->>>>>>> origin/index-swiper
   }
 }
 </script>
