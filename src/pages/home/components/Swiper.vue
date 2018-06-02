e1d7785fa7b80f9d961b8b30965f61630b5ba8e0
falltints <2832731037@qq.com>
2018-06-02 12:17:43 +0800

    change

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl">
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '001',
+        imgUrl: './static/images/swiper-1.jpg'
+      }, {
+        id: '002',
+        imgUrl: './static/images/swiper-2.jpg'
+      }]
+    }
+  }
+}
+</script>
+<style lang="stylus" type="text/stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #ffffff
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 31.25%
+    background: #eeeeee
+    .swiper-img
+      width: 100%
+</style>
