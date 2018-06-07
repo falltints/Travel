5c28d70a05df0ef7c070faa954b1ac3d8cd0782d
falltints <2832731037@qq.com>
2018-06-07 18:14:27 +0800

    finish city-search-logic

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,11 +1,57 @@
 <template>
-  <div class="search">
-    <input type="text" placeholder="输入城市名或拼音" class="search-input">
+  <div>
+    <div class="search">
+      <input type="text" placeholder="输入城市名或拼音" class="search-input" v-model="keyword">
+    </div>
+    <div class="search-content" ref="search" v-show="keyword">
+      <ul>
+        <li v-for="item of list" :key="item.id" class="search-item border-bottom">{{ item.name }}</li>
+        <li class="search-item border-bottom" v-show="hasNoData">没有找到匹配数据</li>
+      </ul>
+    </div>
   </div>
 </template>
 <script>
+import BScroll from 'better-scroll'
 export default {
-  name: 'CitySearch'
+  name: 'CitySearch',
+  props: {
+    cities: Object
+  },
+  data () {
+    return {
+      keyword: '',
+      list: [],
+      timer: null
+    }
+  },
+  computed: {
+    hasNoData () {
+      return !this.list.length
+    }
+  },
+  watch: {
+    keyword () {
+      // 函数节流
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+        for (let i in this.cities) {
+          this.cities[i].forEach((value) => {
+            if (value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1) {
+              result.push(value)
+            }
+          })
+        }
+        this.list = result
+      }, 100)
+    }
+  },
+  mounted () {
+    this.scroll = new BScroll(this.$refs.search)
+  }
 }
 </script>
 <style lang="stylus" type="text/stylus" scoped>
@@ -23,4 +69,18 @@ export default {
     border-radius .06rem
     color #666
     padding 0 .1rem
+.search-content
+  position absolute
+  overflow hidden
+  left 0
+  top 1.58rem
+  right 0
+  bottom 0
+  z-index 1
+  background-color #eee
+  .search-item
+    line-height .62rem
+    padding-left .2rem
+    color #666
+    background-color #ffffff
 </style>
