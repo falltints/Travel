95e13e320f2ddaff15b36dc7c43cfaa6acc7ba7d
falltints <2832731037@qq.com>
2018-06-07 09:58:45 +0800

    finish city-list css part

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,34 @@
+<template>
+  <ul class="list">
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+  </ul>
+</template>
+<script>
+export default {
+  name: 'CityAlphabet'
+}
+</script>
+<style lang="stylus" type="text/stylus" scoped>
+@import '~styles/variable.styl'
+.list
+  display flex
+  flex-direction column
+  justify-content center
+  position absolute
+  right 0
+  top 1.58rem
+  width .4rem
+  color $bgColor
+  .item
+    text-align center
+    line-height .4rem
+</style>
