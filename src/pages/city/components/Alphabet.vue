67544be202c4eca3f067f4d39660db79f1dd8606
falltints <2832731037@qq.com>
2018-06-07 17:12:42 +0800

    finish city-components

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,6 +1,15 @@
 <template>
   <ul class="list">
-    <li class="item" v-for="(item, key) of cities" :key="key">{{ key }}</li>
+    <li
+      class="item"
+      v-for="item of letters"
+      :key="item"
+      @click="handleLetterClick"
+      @touchstart="handleTouchStart"
+      @touchmove="handleTouchMove"
+      @touchend="handleTouchEnd"
+      :ref="item"
+    >{{ item }}</li>
   </ul>
 </template>
 <script>
@@ -8,6 +17,51 @@ export default {
   name: 'CityAlphabet',
   props: {
     cities: Object
+  },
+  data () {
+    return {
+      touchStatus: false,
+      startY: 0,
+      timer: null
+    }
+  },
+  computed: {
+    letters () {
+      const letters = []
+      for (let i in this.cities) {
+        letters.push(i) // i是对象的键
+      }
+      return letters
+    }
+  },
+  methods: {
+    handleLetterClick (e) {
+      this.$emit('change', e.target.innerHTML)
+    },
+    handleTouchStart () {
+      this.touchStatus = true
+    },
+    handleTouchMove (e) {
+      if (this.touchStatus) {
+        // 函数节流
+        if (this.timer) {
+          clearTimeout(this.timer)
+        }
+        this.timer = setTimeout(() => {
+          const touchY = e.touches[0].clientY - 79 // 当前触摸点相对于当前可视区域（窗口）的偏移量
+          const index = Math.floor((touchY - this.startY) / 20) // 每个字母项的高度为20px
+          if (index >= 0 && index < this.letters.length) {
+            this.$emit('change', this.letters[index])
+          }
+        }, 16)
+      }
+    },
+    handleTouchEnd () {
+      this.touchStatus = false
+    }
+  },
+  updated () {
+    this.startY = this.$refs['A'][0].offsetTop // offsetTop是当前元素距离已定位父元素的偏移量
   }
 }
 </script>
@@ -15,10 +69,11 @@ export default {
 @import '~styles/variable.styl'
 .list
   display flex
-  flex-direction column
+  flex-direction column /*使列表项垂直排列*/
   justify-content center
   position absolute
   right 0
+  bottom 0
   top 1.58rem
   width .4rem
   color $bgColor
