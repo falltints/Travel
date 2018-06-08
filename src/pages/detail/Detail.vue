61eff5bc537776471e2ec38d6b1139049345b1bb
falltints <2832731037@qq.com>
2018-06-08 21:18:51 +0800

    error

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -8,11 +8,44 @@
 <script>
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+import axios from 'axios'
 export default {
   name: 'Detail',
   components: {
     DetailBanner,
     DetailHeader
+  },
+  data () {
+    return {
+      sightName: '',
+      bannerImg: '',
+      galleryImgs: [],
+      categoryList: []
+    }
+  },
+  methods: {
+    getDetailInfo () {
+      /* axios.get('/api/detail.json?id=' + this.$route.params.id) */
+      axios.get('/api/detail.json', {
+        params: {
+          id: this.$route.params.id
+        }
+      })
+        .then(this.handleGetDataSucc)
+    },
+    handleGetDataSucc (res) {
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        this.sightName = data.sightName
+        this.bannerImg = data.bannerImg
+        this.galleryImgs = data.galleryImgs
+        this.categoryList = data.categoryList
+      }
+    }
+  },
+  mounted () {
+    this.getDetailInfo()
   }
 }
 </script>
