d00313901dbbfcbe26119a03e6a2265b0a676ebc
falltints <2832731037@qq.com>
2018-06-09 11:18:45 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <keep-alive exclude="Detail">
+    <keep-alive>
       <!--exclude取消缓存--><!--路由被加载一次后，路由中的内容就会被放在内存之中，切换路由不会重新进行渲染-->
       <router-view/><!--router-view显示当前路由地址对应的内容-->
     </keep-alive>
